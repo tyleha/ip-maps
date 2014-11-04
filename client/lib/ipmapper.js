99013388dd37a0d1182ff56266b4696966cb84ca
Tyler Hartley <tyleha@gmail.com>
2014-11-03 18:18:42 -0800

    adding improved comments

--- client/lib/ipmapper.js
+++ client/lib/ipmapper.js
@@ -1,7 +1,11 @@
 IPMapper = {
+
 	map: null,
+
 	latlngbound: null,
+
 	infowindow: null,
+	
 	// baseUrl: "http://freegeoip.net/json/",
 	baseUrl: "http://www.telize.com/geoip/",
 
@@ -25,17 +29,20 @@ IPMapper = {
 		});
 	},
 	parseAndPlotIPAddress: function(ip){
-		ip = $.trim(ip);
+		ip = $.trim(ip); // remove whitespace
 		ipRegex = /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/;
-		if(ip != '' && ipRegex.test(ip)){ //validate IP Address format
-			var url = encodeURI(IPMapper.baseUrl + ip + "?callback=?"); //"?callback=?"); //geocoding url
-			$.getJSON(url, function(data) { //get Geocoded JSONP data
+		// If the IP address fits the test regex pattern,
+		if(ip != '' && ipRegex.test(ip)){ 
+			var url = encodeURI(IPMapper.baseUrl + ip + "?callback=?"); 
+			// Send GET request to telize for geocoded JSONP data
+			$.getJSON(url, function(data) { 
 				if($.trim(data.latitude) != '' && data.latitude != '0' && !isNaN(data.latitude)){ //Geocoding successfull
-
+					// Parse all key, val pairs to place inside the marker window on click
 					var contentString = "";
 					$.each(data, function(key, val) {
 						contentString += '<b>' + key.toUpperCase().replace("_", " ") + ':</b> ' + val + '<br />';
 					});
+					// Add this IP address and geolocation to the mongodb
 					IPAddresses.insert({
 			          ip: ip,
 			          createdAt: new Date(), // current time
@@ -45,7 +52,7 @@ IPMapper = {
 			          city: data.city,
 			          country: data.country,
 			        });
-
+					// Actually go about plotting on a map
 			        IPMapper.addIPMarker(data.latitude, data.longitude, contentString)
 				} else {
 					IPMapper.logError('IP Address geocoding failed!');
@@ -57,18 +64,20 @@ IPMapper = {
 			IPMapper.logError('Invalid IP Address!');
 			$.error('Invalid IP Address!');
 		}
-		
 	},
 	addIPMarker: function(latitude, longitude, contentString){
+		// Display the marker on the map
 		var latlng = new google.maps.LatLng(latitude, longitude);
 		var marker = new google.maps.Marker({ //create Map Marker
 			map: IPMapper.map,
 			draggable: false,
 			position: latlng
 		});
-		IPMapper.placeIPMarker(marker, latlng, contentString); //place Marker on Map
+		// place Marker on Map
+		IPMapper.placeIPMarker(marker, latlng, contentString); 
 	},
-	placeIPMarker: function(marker, latlng, contentString){ //place Marker on Map
+	placeIPMarker: function(marker, latlng, contentString){ 
+		//place Marker on Map
 		marker.setPosition(latlng);
 		google.maps.event.addListener(marker, 'click', function() {
 			IPMapper.getIPInfoWindowEvent(marker, contentString);
@@ -77,7 +86,8 @@ IPMapper = {
 		IPMapper.map.setCenter(IPMapper.latlngbound.getCenter());
 		IPMapper.map.fitBounds(IPMapper.latlngbound);
 	},
-	getIPInfoWindowEvent: function(marker, contentString){ //open Marker Info Window
+	getIPInfoWindowEvent: function(marker, contentString){ 
+		//open Marker Info Window
 		IPMapper.infowindow.close()
 		IPMapper.infowindow.setContent(contentString);
 		IPMapper.infowindow.open(IPMapper.map, marker);
--- ip-maps.js
+++ ip-maps.js
@@ -12,8 +12,11 @@ if (Meteor.isClient) {
     "submit .new-ip": function (event) {
       // This function is called when the new IP address form is submitted
       var text = event.target.text.value;
+      // Split entered IP addresses by comma
       var addresses = text.split(',');
+      // For each bit of text separated by a comma,
       _.each(addresses, function(addr) {
+        // parse, check ip address validity, and plot on the map
         IPMapper.parseAndPlotIPAddress(addr);
       });
       
@@ -26,19 +29,22 @@ if (Meteor.isClient) {
   });
 
   Template.iplist.events({
+    // If someone clicks the delete button
     "click .delete": function () {
       IPAddresses.remove(this._id);
       // Re-initialize map to clear all markers (TODO: remove specific marker)
       IPMapper.initializeMap("map");
+      // Plot each ip address still in the database
       var ips = IPAddresses.find().fetch();
       _.each(ips, function(ip) {
+          // Already have serialized lat and lon - bypass parseAndPlot
           IPMapper.addIPMarker(ip.latitude, ip.longitude, ip.contentString);
       });
     }
   });
 
   Template.ipmap.rendered = function(){
-
+    // If we haven't rendered the map before
     if (! Session.get('map'))
       IPMapper.initializeMap("map");
       Session.set('map', true)
@@ -47,6 +53,7 @@ if (Meteor.isClient) {
       // Iterate through the existing IP addresses and plot
       var ips = IPAddresses.find().fetch();
       _.each(ips, function(ip) {
+        // Already have serialized lat and lon - bypass parseAndPlot
         IPMapper.addIPMarker(ip.latitude, ip.longitude, ip.contentString);
       });
     });
